badb682632a76f7a59e9686eea544e0ea647050a
Jayzzer <pavel10_pushkarev@mail.ru>
2019-11-18 00:14:53 +0300

    Initial commit

---
+++ src/main/java/com/pupsiki/digitallibrary/DigitalLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.pupsiki.digitallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DigitalLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalLibraryApplication.class, args);
+    }
+}
