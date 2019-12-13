3e77bfcb1eb818093236ec627ab1862b39015cfe
Jayzzer <pavel10_pushkarev@mail.ru>
2019-12-14 01:33:05 +0300

    Done book catalog. Add book displayin. Add sorting. Add pagination. Add categories caching

--- src/main/java/com/pupsiki/digitallibrary/repositories/BookRepository.java
+++ src/main/java/com/pupsiki/digitallibrary/repositories/BookRepository.java
@@ -1,12 +1,17 @@
 package com.pupsiki.digitallibrary.repositories;
 
 import com.pupsiki.digitallibrary.models.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface BookRepository extends CrudRepository<Book, Integer> {
+public interface BookRepository extends CrudRepository<Book, Integer>, PagingAndSortingRepository<Book, Integer> {
     List<Book> findTop10ByOrderByRatingDesc();
     List<Book> findTop10ByOrderByCreatedAtDesc();
     List<Book> findTop10ByPriceOrderByRatingDesc(float price);
+
+    Page<Book> findAllByGenre(String genre, Pageable pageable);
 }
